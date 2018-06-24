3d7929a978220abc34778c5419b49b80556fcc02
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-24 01:23:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,7 +23,7 @@ class School
     end
 
     def sort
-        @roster.each {|i, e| e.sort}
-        @roster.sort.to_h
+        @roster.each {|i, e| e.sort}.sort.to_h
+        # @roster.sort.to_h
     end
 end
