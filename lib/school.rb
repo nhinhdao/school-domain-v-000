7b73290497d810fa9a32b4da6d10b44fc4d86b3f
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-24 01:10:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+    attr_reader :name, :roster
+    attr_accessor :roster
+    # SCHOOL = {}
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, num)
+        @roster[num] = []
+        if @roster.keys.include?(num)
+            @roster[num] = []
+            @roster[num] << name
+        else
+            @roster[num] << name
+        end
+        @roster
+    end
+
+    def grade(num)
+        @roster.values_at(num)
+    end
+
+    def sort
+        @roster.sort
+    end
+end
